d5b466a20221cd315e7d3341310aaa4ba4d8810a
Anandhakrishnan15 <anandhakrishnanvellat15@gmail.com>
2025-08-09 18:58:50 +0530

    adde all the comnts forthe home page

--- frountend/src/app/page.js
+++ frountend/src/app/page.js
@@ -1,5 +1,14 @@
 import Link from "next/link";
 import Demo from "../components/hero/Demo.jsx";
+import { QuickIntroUSP } from "../components/QuickIntroUSP.jsx";
+import { TopServicesPreview } from "../components/TopServicesPreview.jsx";
+import { Testimonials } from "../components/Testimonials.jsx";
+import { PromoOffer } from "../components/PromoOffer.jsx";
+import { BlogTips } from "../components/BlogTips.jsx";
+import { CTASection } from "../components/CTASection.jsx";
+import { ContactSocial } from "../components/ContactSocial.jsx";
+import { BeforeAfter } from "../components/BeforeAfter.jsx";
+import CertificateCarousel from "../components/Certifications.jsx";
 // import Hero from "../components/Hero.jsx"
 
 export default function Home() {
@@ -7,6 +16,15 @@ export default function Home() {
     <div>
       {/* <Hero /> */}
       <Demo />
+      <QuickIntroUSP />
+      {/* <TopServicesPreview /> */}
+      <BeforeAfter />
+      <Testimonials />
+      <CertificateCarousel />
+      <PromoOffer />
+      <BlogTips />
+      <CTASection />
+      <ContactSocial />
       
     </div>
   );
