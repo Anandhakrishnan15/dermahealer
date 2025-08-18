9266781746f6abac3b70e2b0bc28d59fb13c1409
Anandhakrishnan15 <anandhakrishnanvellat15@gmail.com>
2025-08-18 22:50:49 +0530

    catoger and slug updated

--- frountend/src/data/serviciess.jsx
+++ frountend/src/data/serviciess.jsx
@@ -2,7 +2,7 @@ const products = [
     {
         title: "HIFU (High-Intensity Focused Ultrasound) – Non-Surgical Facelift",
         category: "face",   // 👈 add category
-        slug: "acne-scar",
+        slug: "anti-ageing",
       
         thumbnail: "https://media.post.rvohealth.io/wp-content/uploads/2018/12/Can-High-Intensity-Focused-Ultrasound-Treatment-Replace-Face-Lifts-_1200x628-facebook.jpg",
         description:
@@ -18,16 +18,16 @@ const products = [
     },
     {
         title: "Laser Hair Removal – Safe & Permanent",
-        category: "face",   // 👈 add category
-        slug: "acne-scar",
+        category: "hair",   // 👈 add category
+        slug: "laser-hair-removal",
         thumbnail: "https://www.kolorshealthcare.com/blog/wp-content/uploads/2023/07/Laser-Hair-Removal-Face.jpg",
         description:
             "Get freedom from waxing and shaving with USFDA-approved laser hair removal. Suitable for all skin types and both men and women.",
     },
     {
         title: "HydraFacial – Instant Glow & Deep Cleansing",
-        category: "face",   // 👈 add category
-        slug: "acne-scar",
+        category: "body",   // 👈 add category
+        slug: "beauty-services",
         thumbnail: "https://zazzlesalon.com/wp-content/uploads/2023/04/Hydra-Facial.jpg",
         description:
             "A globally loved facial that detoxifies, exfoliates, hydrates, and nourishes your skin—all in one session. Ideal for dull, dry, and sensitive skin types.",
@@ -35,47 +35,47 @@ const products = [
     {
         title: "Hyperpigmentation Treatment",
         category: "face",   // 👈 add category
-        slug: "acne-scar",
+        slug: "pigmentation",
         thumbnail: "https://drnerinawilkinson.co.za/wp-content/uploads/2024/03/Newsletter-images-5-scaled.jpg",
         description:
             "We treat melasma, sun spots, freckles and uneven skin tone with customized peels, lasers, and serums that target the pigmentation.",
     },
     {
         title: "Hair Loss Treatment",
-        category: "face",   // 👈 add category
-        slug: "acne-scar",
+        category: "hair",   // 👈 add category
+        slug: "hair-loss",
         thumbnail: "https://skin111.com/uploads/image/1723537137Hair_Loss_Treatment_for_Men.webp",
         description:
             "Get fuller, healthier hair with our PRP, mesotherapy, and growth factor treatments tailored to your scalp’s needs. Ideal for men and women with thinning hair.",
     },
     {
         title: "Fat Loss Treatment – Non-Surgical Body Shaping",
-        category: "face",   // 👈 add category
-        slug: "acne-scar",
+        category: "body",   // 👈 add category
+        slug: "body-treatment",
         thumbnail: "https://perfectskincenter.com/wp-content/uploads/2023/06/638e1ccf5d.jpg",
         description:
             "Target stubborn fat in belly, thighs, arms, or love handles with our non-invasive fat reduction treatments with HDPEM technology.",
     },
     {
         title: "Urinary Incontinence Treatment (Kegel Chair Therapy)",
-        category: "face",   // 👈 add category
-        slug: "acne-scar",
+        category: "",   // 👈 add category
+        slug: "",
         thumbnail: "https://www.kaya.in/media/mageplaza/blog/post/s/a/safe-effective-body-sculpting-fat-reduction.jpg",
         description:
             "A revolutionary non-invasive treatment for post-pregnancy and age-related urinary leakage using the latest electromagnetic chair technology.",
     },
     {
         title: "Vitiligo Surgery (Punch Grafting & Suction Blister Grafting)",
-        category: "face",   // 👈 add category
-        slug: "acne-scar",
+        category: "skin",   // 👈 add category
+        slug: "vitiligo",
         thumbnail: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQFJQAeYvEPNzpvDGugXIeSrHhsv-eN_1ylA&s",
         description:
             "We offer effective surgical options for stable vitiligo patches using advanced techniques to restore pigmentation safely and successfully.",
     },
     {
         title: "Laser Tattoo Removal – Safe & Effective",
-        category: "face",   // 👈 add category
-        slug: "acne-scar",
+        category: "",   // 👈 add category
+        slug: "",
         thumbnail: "https://www.kaya.in/media/mageplaza/blog/post/l/a/laser-tattoo-removal.jpg",
         description:
             "Our Q-switched laser gently breaks down tattoo pigments without damaging your skin, removing unwanted tattoos with precision and minimal scarring.",
