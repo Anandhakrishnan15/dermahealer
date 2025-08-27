ccb1d41cd22e1dcc0acb760deba5859571819b28
Anandhakrishnan15 <anandhakrishnanvellat15@gmail.com>
2025-08-27 15:23:44 +0530

    navbar dreopdown data

---
+++ frountend/src/data/navbarDropdoenData.js
@@ -0,0 +1,191 @@
+// src/data/navbarDropdownData.js
+const navbarDropdownData = [
+    {
+        label: "Face Treatments",
+        items: [
+            {
+                label: "Acne & Acne Scar Treatments",
+                href: "/treatments/skin/acne",
+            },
+            {
+                label: "Anti-Ageing & Skin Rejuvenation",
+                items: [
+                    {
+                        label: "HydraFacial",
+                        href: "/treatments/face/hydrafacial",
+                    },
+                    {
+                        label: "Laser Toning",
+                        href: "/treatments/face/laser-toning",
+                    },
+                    {
+                        label: "Carbon Laser Toning",
+                        href: "/treatments/face/carbon-laser-toning",
+                    },
+                    {
+                        label: "HIFU",
+                        href: "/treatments/face/hifu",
+                    },
+                    {
+                        label: "Vampire Facial",
+                        href: "/treatments/face/vampire-facial",
+                    }
+
+                ],
+            },
+            {
+                label: "Pigmentation",
+                items: [
+                    {
+                        label: "Melasma Treatment",
+                        href: "/treatments/skin/pigmentation",
+                    },
+                    {
+                        label: "Freckle Remove",
+                        href: "/treatments/skin/pigmentation",
+                    },
+
+                ],
+            },
+            {
+                label: "Xanthelasma Removal",
+                href: "/treatments/face/xanthelasma-removal",
+            }
+
+
+        ],
+    },
+    {
+        label: "Hair Treatments",
+        items: [
+            {
+                label: "Laser Hair Removal",
+                href: "/treatments/hair/laser-hair-removal",
+            },
+            {
+                label: "Hair Loss Treatments",
+                items: [
+                    {
+                        label: "PRP",
+                        href: "/treatments/hair/prp-therapy",
+                    },
+                    {
+                        label: "GFC",
+                        href: "/treatments/hair/gfc-therapy",
+                    },
+                    {
+                        label: "Mesotherapy",
+                        href: "/treatments/face/mesotherapy-skin-boosters",
+                    },
+                    {
+                        label: "Growth factors",
+                        href: "/treatments/hair/growth-factors",
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        label: "Skin & Lesion Treatments",
+        items: [
+            {
+                label: "Lesion Removal",
+                items: [
+                    {
+                        label: "Mole Removal",
+                        href: "/treatments/skin/mole-removal",
+                    },
+                    {
+                        label: "wart Removal",
+                        href: "/treatments/skin/wart-removal",
+                    },
+                ],
+            },
+            {
+                label: "Vitiligo Treatments",
+                href: "/treatments/skin/vitiligo",
+            },
+
+        ],
+    },
+    {
+        label: "Body Treatments",
+        items: [
+            {
+                label: "HIFU Body",
+                href: "/treatments/face/hifu",
+            },
+            {
+                label: "Emshape",
+                href: "/treatments/body/ems-shape",
+            },
+
+        ],
+    },
+    {
+        label: "Beauty Services",
+        items: [
+
+            {
+                label: "HydraFacial",
+                href: "/treatments/face/hydrafacial",
+            },
+            {
+                label: "Laser Toning",
+                href: "/treatments/face/laser-toning",
+            },
+            {
+                label: "Carbon Laser Toning",
+                href: "/treatments/face/carbon-laser-toning",
+            },
+            {
+                label: "Vampire Facial",
+                href: "/treatments/face/vampire-facial",
+            },
+            {
+                label: "Medi Facial",
+                href: "/treatments/skin/medi-facial",
+            }
+
+
+        ],
+
+    },
+    {
+        label: "Wellness & Health",
+        items: [
+
+            {
+                label: "IV Drops",
+                href: "/treatments/Wellness-Health/iv-therapy",
+            },
+            {
+                label: "Urinary Incontinence",
+                href: "/treatments/Wellness-Health/urinary-incontinence",
+            },
+        ],
+    },
+    {
+        label: "Sexual Health",
+        items: [
+            {
+                label: "vaginal tightenis",
+                href: "/treatments/Sexual-Health/vaginal-tightening",
+            },
+            {
+                label: "premwture ejacution Treatment",
+                href: "/treatments/Sexual-Health/premature-ejaculation",
+            },
+            {
+                label: "Genital Wort removal",
+                href: "/treatments/Sexual-Health/genital-wart-removal",
+            },
+        ],
+
+    },
+    {
+        label: "Tattoo Removal",
+        href: "/treatments/tattoo-removal",
+    },
+];
+export default navbarDropdownData
