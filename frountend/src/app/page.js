ac9eb419192c593b2716bd056d89a49fcdc1f256
Anandhakrishnan15 <anandhakrishnanvellat15@gmail.com>
2025-09-27 14:04:02 +0530

    home page upgrade

--- frountend/src/app/page.js
+++ frountend/src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
 
 import Demo from "../components/hero/Demo.jsx";
-import { QuickIntroUSP } from "../components/QuickIntroUSP.jsx";
 import { TopServicesPreview } from "../components/TopServicesPreview.jsx";
 import { Testimonials } from "../components/Testimonials.jsx";
 import { BlogTips } from "../components/BlogTips.jsx";
@@ -10,12 +9,19 @@ import CertificateCarousel from "../components/Certifications.jsx";
 import BeforeAfterSlideshow from "../components/BeforeAfter.jsx";
 import treatments from "@/data/BeforAndAfter.jsx";
 import OurExperts from "../components/AnimatedCounter.jsx";
+import { HeroParallaxContent } from "@/components/hero/HeroParallaxContent.jsx";
+import ResponsiveImageGallery from "@/components/ResponsiveImageGallery.jsx";
+import products from "@/data/serviciess.jsx";
+import { HeroParallax } from "@/components/hero/HeroParallax.jsx";
 
 export default function Home() {
  
   return (
     <div>
-      <Demo />
+      {/* <Demo /> */}
+      <HeroParallax products={products} />;
+      <ResponsiveImageGallery />
+      <HeroParallaxContent/>
       <TopServicesPreview limit={3} showSeeMore />
       <BeforeAfterSlideshow data={treatments} />
       <Testimonials />
