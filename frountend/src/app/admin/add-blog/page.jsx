904f724f4f0ef57365ef55ca74e7623b7fde54e0
Anandhakrishnan15 <anandhakrishnanvellat15@gmail.com>
2026-04-05 14:57:56 +0530

    fixing the Blogpage auth for admin

--- frountend/src/app/admin/add-blog/page.jsx
+++ frountend/src/app/admin/add-blog/page.jsx
@@ -1,11 +1,15 @@
 "use client";
 
 import BlogsPage from "@/components/Admin/AdminDashboard";
+import { useAuth } from "@/context/AuthContext";
+import { redirect } from "next/navigation";
 
 export default function adminblogPage() {
-
+const { user } = useAuth();
+    if (!user || user.role !== "admin") {
+        redirect("/"); // or show 403 page
+    }
     return (
-        
         <>
             <BlogsPage />
         </>
