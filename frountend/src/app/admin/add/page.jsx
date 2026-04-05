e66b31af0a9b176108b9787bcc09d65c123b41a6
Anandhakrishnan15 <anandhakrishnanvellat15@gmail.com>
2026-04-05 14:55:17 +0530

    page of add data form

---
+++ frountend/src/app/admin/add/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import AddForm from "./components/AddForm";
+
+export default function AddPatientPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-100 via-cyan-50 to-blue-100">
+      <AddForm />
+    </div>
+  );
+}
