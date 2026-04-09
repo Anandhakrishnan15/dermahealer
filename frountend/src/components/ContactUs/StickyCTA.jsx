908cd013b27506d2ac59c9330e93a4c4e87bcf57
Anandhakrishnan15 <anandhakrishnanvellat15@gmail.com>
2026-04-09 21:40:36 +0530

    stiker CtA updeed

--- frountend/src/components/ContactUs/StickyCTA.jsx
+++ frountend/src/components/ContactUs/StickyCTA.jsx
@@ -2,7 +2,7 @@ import { PhoneCall, Calendar } from "lucide-react";
 
 export function StickyCTA() {
     return (
-        <div className="fixed bottom-6 right-6 flex flex-col space-y-4 z-50">
+        <div className="fixed bottom-6 left-6 flex flex-col space-y-4 z-50">
             {/* Call Now */}
             <a
                 href="tel:+1234567890"
